b2bcdc4b4e2672849590f7ff50b37c3938b69420
rbaris <80262651+rbaris@users.noreply.github.com>
2021-06-03 14:04:15 +0300

    Add files via upload

---
+++ Web Programlama/version_3/myjavascript.js
@@ -0,0 +1,63 @@
+          //1.yazı yönlendirme işlemi
+           function yazi1(adres) 
+           {
+        window.open(adres, "_blank");
+           }
+           function yazi1Ac()
+           {
+        yazi1("yazi1.html");
+           }
+           //2. yazı yönlendirme işlemi
+            function yazi2(adres) 
+           {
+        window.open(adres, "_blank");
+           }
+           function yazi2Ac()
+           {
+        yazi2("yazi2.html");
+           }
+            //3. yazı yönlendirme işlemi
+            function yazi3(adres) 
+           {
+        window.open(adres, "_blank");
+           }
+           function yazi3Ac()
+           {
+        yazi3("yazi3.html");
+           }
+           //4. yazı yönlendirme işlemi
+            function yazi4(adres) 
+           {
+        window.open(adres, "_blank");
+           }
+           function yazi4Ac()
+           {
+        yazi4("yazi4.html");
+           }
+           //5. yazı yönlendirme işlemi
+            function yazi5(adres) 
+           {
+        window.open(adres, "_blank");
+           }
+           function yazi5Ac()
+           {
+        yazi5("yazi5.html");
+           }
+           //6. yazı yönlendirme işlemi
+            function yazi6(adres) 
+           {
+        window.open(adres, "_blank");
+           }
+           function yazi6Ac()
+           {
+        yazi6("yazi6.html");
+           }
+          //7. yazı yönlendirme işlemi
+            function yazi7(adres) 
+           {
+        window.open(adres, "_blank");
+           }
+           function yazi7Ac()
+           {
+        yazi7("yazi7.html");
+           }
